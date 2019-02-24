09645c34e9106aa8dffbd3a912a561e2eb6d20c9
Yury Kiryla <yury_kiryla@epam.com>
2019-02-24 14:07:24 -0500

    add hystrix

--- itemsharing/itemsharing.itemservice/src/main/java/com/itemsharing/itemservice/service/impl/ItemServiceImpl.java
+++ itemsharing/itemsharing.itemservice/src/main/java/com/itemsharing/itemservice/service/impl/ItemServiceImpl.java
@@ -3,6 +3,7 @@ package com.itemsharing.itemservice.service.impl;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,8 @@ import com.itemsharing.itemservice.model.User;
 import com.itemsharing.itemservice.repository.ItemRepository;
 import com.itemsharing.itemservice.service.ItemService;
 import com.itemsharing.itemservice.service.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -26,23 +29,23 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	UserFeignClient userFeignClient;
 
 	@Override
 	public Item addItemByUser(Item item, String username) {
 		Item localItem = itemRepository.findByName(item.getName());
-		
+
 		if (localItem != null) {
 			LOG.info("Item with name {} already exists. Nothing will be done.", localItem.getName());
 		} else {
 			Date today = new Date();
 			item.setAddDate(today);
-			
+
 			User user = userService.getUserByUsername(username);
 			item.setUser(user);
-			
+
 			localItem = itemRepository.save(item);
 			return localItem;
 		}
@@ -57,7 +60,7 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public List<Item> getItemsByUsername(String username) {
 		User user = userService.getUserByUsername(username);
-		
+
 		return itemRepository.findByUser(user);
 	}
 
@@ -69,7 +72,7 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public Item updateItem(Item item) throws IOException {
 		Item localItem = getItemById(item.getId());
-		
+
 		if (localItem == null) {
 			throw new IOException("Item was not found.");
 		} else {
@@ -77,7 +80,7 @@ public class ItemServiceImpl implements ItemService {
 			localItem.setItemCondition(item.getItemCondition());
 			localItem.setStatus(item.getStatus());
 			localItem.setDescription(item.getDescription());
-			
+
 			return itemRepository.save(localItem);
 		}
 	}
@@ -88,9 +91,45 @@ public class ItemServiceImpl implements ItemService {
 	}
 
 	@Override
+//	@HystrixCommand(commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "12000") })
+	@HystrixCommand(
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")},
+			fallbackMethod = "buildFallbackUser", 
+			threadPoolKey = "itemByUserThreadPool",
+			threadPoolProperties = {
+					@HystrixProperty(name = "coreSize", value = "30"),
+					@HystrixProperty(name = "maxQueueSize", value = "10")
+					}
+			)
 	public User getUserByUsername(String username) {
 //		return userService.getUserByUsername(username);
+
+		randomlyRunLong();
+
 		return userFeignClient.getUserByUsername(username);
 	}
 
+	private void randomlyRunLong() {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+		if (randomNum == 3) {
+			sleep();
+		}
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(11000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private User buildFallbackUser(String username) {
+		User user = new User();
+		user.setId(123123L);
+		user.setUsername("Temp Username");
+		
+		return user;
+	}
 }
