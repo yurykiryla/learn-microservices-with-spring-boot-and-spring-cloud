0b3983d6fd5db01fa84a892a9cc326ba5c48910c
Yury Kiryla <yury_kiryla@epam.com>
2019-02-28 11:02:19 -0500

    add authorization server

---
+++ itemsharing/itemsharing.authorizationserver/src/main/java/com/itemsharing/authorizationserver/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.itemsharing.authorizationserver.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.itemsharing.authorizationserver.model.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	User findByUsername(String username);
+
+}
---
+++ itemsharing/itemsharing.authorizationserver/src/main/java/com/itemsharing/authorizationserver/security/UserSecurityService.java
@@ -0,0 +1,34 @@
+package com.itemsharing.authorizationserver.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.itemsharing.authorizationserver.model.User;
+import com.itemsharing.authorizationserver.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		
+		if (user == null) {
+			LOG.warn("Username {} was not found", username);
+			throw new UsernameNotFoundException("Username " + username + " not found");
+		}
+				
+		return user;
+	}
+
+}
